0cc5fb5d8ac55fc20d9c57de3693abe9e19f0a72
gokulgithub11 <53489975+gokulgithub11@users.noreply.github.com>
2019-08-28 23:32:24 +0530

    Add files via upload

--- cucumber.java
+++ cucumber.java
@@ -19,12 +19,13 @@ public class cucumber {
 System.setProperty("webdriver.chrome.driver","C:\\Users\\ELCOT\\eclipse-workspace\\Cucumberstart\\Driver\\chromedriver.exe");
 driver =new ChromeDriver();
 driver.get("http://demo.guru99.com/telecom/");
+driver.manage().window().maximize();
 	}
 
-	@Given("user add cusomter link")
+	/*@Given("user add cusomter link")
 	public void user_add_cusomter_link() {
 	driver.findElement(By.xpath("(//a[text()='Add Customer'])[1]")).click();	
-		}
+		}*/
 
 	/*@When("user provide valid ditails")
 	public void user_provide_valid_ditails() {
@@ -63,7 +64,7 @@ driver.get("http://demo.guru99.com/telecom/");
 		driver.findElement(By.xpath("//input[@name='submit']")).click();	
 	}*/
 	
-	@When("user provide valid ditails")
+	/*@When("user provide valid ditails")
 	public void user_provide_valid_ditails(io.cucumber.datatable.DataTable dataTable) {
 		Map<String,String>onedmap=dataTable.asMap(String.class,String.class);
 		System.out.println(onedmap);
@@ -80,6 +81,28 @@ driver.get("http://demo.guru99.com/telecom/");
 	public void verfiy_the_customer_ID_is_displayed() {
 		Assert.assertTrue(driver.findElement(By.xpath("(//td[@align='center'])[2]")).isDisplayed());
 			
+	}*/
+	
+	@Given("user should clik add tariff plan")
+	public void user_should_clik_add_tariff_plan() {
+		  driver.findElement(By.xpath("//a[text()='Add Tariff Plan']")).click();
+				  
+	}
+
+	@When("user provide valid input and sumbit")
+	public void user_provide_valid_input_and_sumbit(io.cucumber.datatable.DataTable dataTable) {
+		List<List<String>> twodimen = dataTable.asLists(String.class);
+		driver.findElement(By.id("rental1")).sendKeys(twodimen.get(0).get(0));
+	    driver.findElement(By.id("local_minutes")).sendKeys(twodimen.get(0).get(1));
+	    driver.findElement(By.id("inter_minutes")).sendKeys(twodimen.get(0).get(2));
+	    driver.findElement(By.id("sms_pack")).sendKeys(twodimen.get(1).get(3));
+	    driver.findElement(By.id("minutes_charges")).sendKeys(twodimen.get(1).get(4));
+	    driver.findElement(By.id("inter_charges")).sendKeys(twodimen.get(0).get(5));
+	    driver.findElement(By.id("sms_charges")).sendKeys(twodimen.get(2).get(6));
+	    driver.findElement(By.xpath("//input[@name='submit']")).click();
+	    }
+	@Then("to verify success message is displayed")
+	public void to_verify_success_message_is_displayed() {
+		Assert.assertTrue(driver.findElement(By.xpath("//h2[text()='Congratulation you add Tariff Plan']")).isDisplayed());
 	}
 }
-
